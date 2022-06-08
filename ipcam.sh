0d718aa6c1ed911a9eeebf1028149ea9a0def684
Zijie NING <zijie.ning@imt-atlantique.net>
2022-06-08 15:57:05 +0200

    add ipcam, sweep, small anchors

--- ipcam.sh
+++ ipcam.sh
@@ -1,1 +1,1 @@
-python detect.py --source rtsp://admin:brain2021@10.29.232.35 --weights "yolov5n.pt"
+python detect.py --source rtsp://admin:brain2021@10.29.232.35 --weights "runs/train/exp/weights/best.pt"
---
+++ setup.sh
@@ -0,0 +1,20 @@
+# Setup python venv and install requirement
+# 
+# Copy dataset with
+# scp -v -r smart_cam/ z20ning@pc-elec-389.priv.enst-bretagne.fr:/homes/z20ning/Bureau/Local 
+
+# For a new pc
+# sudo apt upgrade
+# sudo apt update
+# sudo apt-get install python3-venv
+
+# Set path of venv
+cd ~/Bureau/Local
+
+mkdir pyvenv
+cd pyvenv
+python3 -m venv smartcam
+source ~/Bureau/Local/pyvenv/smartcam/bin/activate
+
+cd ~/Bureau/Yolo/yolov5
+pip install -r requirements.txt
